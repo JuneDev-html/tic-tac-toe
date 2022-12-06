dac7d8995084777ef24848fc9e0bc713f6fb6c93
June <adasilva.html@gmail.com>
2022-12-05 23:01:11 -0500

    Add random moves logic to seperate js file
    Will keep logic of random bot moves in seperate file.
    will add it to working game as "easy mode" bot function at later date.

--- scrap.js
+++ scrap.js
@@ -1,33 +1,15 @@
-  //  CHANGE CHANGE CHANGE 
-  // CHANGE CHANGE CHANGE
-  var botMove = function () {
-    var score = 0;
-    var bestScore = 0;
-    var bestMove;
+function botMove() {
+  var trying = true;
+  while (trying) {
+    var random = Math.floor(Math.random() * 8);
+    if (board[random] === '') {
 
-    for (let i = 0; i < 9; i++) {
-      if (board[i] === '') {
-        board[i] = 'O';
-        score = minimax(board, 0, true);
-        board[i] = '';
-        bestScore = max(score, bestScore);
-      }
+      board[random] = 'O';
+      setTimeout(() => {
+        document.querySelector(`[data-value="${random}"`).innerHTML = 'O';
+        botMoving = false; // add this in here so player cant move before bot
+      }, 400);
+      trying = false;
     }
-    board[bestMove] = 'O';
-    setTimeout(() => {
-      document.querySelector(`[data-value="${bestMove}"`).innerHTML = 'O';
-      botMoving = false; // add this in here so player cant move before bot
-    }, 400);
-    console.log(board)
   }
-
-
-function emptyIndexies(board){
-  return  board.filter(s => s != "O" && s != "X");
-}
-
-
-function minimax(newBoard, player) {
-  var availSpots = emptyIndexies(board)
-  console.log(availSpots)
 }
